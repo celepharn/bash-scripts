ce60eecacc87f34ff9436c0c4978a033502f1e82
Celepharn <65320339+celepharn@users.noreply.github.com>
2023-12-09 15:06:29 -0600

    Added image previews.

--- scripts/mem_alert.sh
+++ scripts/mem_alert.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
+# Author: github.com/celepharn
 while true
 do
+	# Grab available ram
 	mem_available=$(cat /proc/meminfo | awk '/MemAvailable/ {print $2}')
+
+	# Set a threshold, modify this to your needs
 	if [ $mem_available -le 500000 ]; then
+		# Convert to MiB
 		mem=$((((mem_available))/1000))
+		# Notify
 		dunstify "🚨 Warning: low RAM" "You got ${mem} MiB left"
 	fi
 	sleep 30
--- scripts/mem_now.sh
+++ scripts/mem_now.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Author: github.com/celepharn
 mem_available=$(cat /proc/meminfo | awk '/MemAvailable/ {print $2}')
 mem=$((((mem_available))/1000))
 dunstify "🍭 Ram sanity check" "You got ${mem} MiB available"
